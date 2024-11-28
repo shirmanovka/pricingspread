0be19e3e00b71ecc6c0f6edd865dd71707b3fd8c
Kirill <187307451+shirmanovka@users.noreply.github.com>
2024-11-28 13:34:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,16 @@ import streamlit as st
 
 st.title('Pricing spread')
 
-df = pd.read_excel(('Карта рынка.xlsx'), skiprows=1)
+df = pd.read_excel('Карта рынка.xlsx', skiprows=1)
 
-s_df=df[['ISIN','Тикер','Рейтинг','Цена, пп', 'Срок  до погашения / оферты, лет']] copy ()
-
+s_df = df[['ISIN','Тикер','Рейтинг','Цена, пп', 'Срок  до погашения / оферты, лет']].copy()
 
 # Фильтры для столбцов
 tickers = s_df['Тикер'].unique()
 selected_tickers = st.multiselect('Выберите тикер:', tickers)
- 
+
 # Фильтрация данных
 f_df = s_df[(s_df['Тикер'].isin(selected_tickers) | (len(selected_tickers) == 0))]
-
 
 # Отображение отфильтрованного DataFrame
 st.dataframe(f_df)
