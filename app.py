fbe604a9f77263a3575017847f4d21d2dceb38cf
Kirill <187307451+shirmanovka@users.noreply.github.com>
2024-11-28 13:53:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ import streamlit as st
 st.title('Pricing spread')
 
 df = pd.read_excel('Карта рынка.xlsx', skiprows=1)
+df['spread'] = (df['Спред, пп'] * 100)
+df['Yield'] = ((100 - df['Цена, пп']) * 100) / df['Срок  до погашения / оферты, лет']
+df['Cupon'] = df['spread'] / df['Цена, пп'] * 100 - df['spread']
+df['Cspread'] = round(df['spread'] + df['Cupon'] + df['Yield'])
 
-s_df = df[['ISIN','Тикер','Рейтинг','Цена, пп', 'Срок  до погашения / оферты, лет']].copy()
+s_df = df[['ISIN','Тикер','Рейтинг','Цена, пп', 'Срок  до погашения / оферты, лет','Cspread']].copy()
 
 # Фильтры для столбцов
 tickers = s_df['Тикер'].unique()
