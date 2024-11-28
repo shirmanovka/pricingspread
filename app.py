d13c2491f725487dd9aef9403b5694cc66617e21
Kirill <187307451+shirmanovka@users.noreply.github.com>
2024-11-28 13:09:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+st.title('Pricing spread')
 
+df = pd.read_excel(('Карта рынка.xlsx'), skiprows=1)
+
+s_df=df['ISIN','Тикер','Рейтинг','Цена, пп', 'Срок  до погашения / оферты, лет',  ]
+
+
+# Фильтры для столбцов
+tickers = s_df['Тикер'].unique()
+selected_tickers = st.multiselect('Выберите тикер:', tickers)
+ 
+# Фильтрация данных
+f_df = df[(s_df['Тикер'].isin(selected_tickers) | (len(selected_tickers) == 0))]
+
+
+# Отображение отфильтрованного DataFrame
+st.dataframe(f_df)
